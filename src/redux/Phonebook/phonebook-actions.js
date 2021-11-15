25e2e1a0cbc36cd28d920c03e60ead2c1ac77e06
Evgeniya Bayda <baje75128@gmail.com>
2021-11-15 11:15:48 +0200

    delete comment

--- src/redux/Phonebook/phonebook-actions.js
+++ src/redux/Phonebook/phonebook-actions.js
@@ -3,17 +3,6 @@
 //reduxjs/toolkit
 import { createAction } from '@reduxjs/toolkit';
 
-// export const fetchContactRequest = createAction('contacts/fetchRequest');
-// export const fetchContactSuccess = createAction('contacts/fetchSuccess');
-// export const fetchContactError = createAction('contacts/fetchError');
-
-// export const addContactRequest = createAction('contacts/addContactRequest');
-// export const addContactSuccess = createAction('contacts/addContactSuccess');
-// export const addContactError = createAction('contacts/addContactError');
-
-// export const deleteContactRequest = createAction('contacts/deleteRequest');
-// export const deleteContactSuccess = createAction('contacts/deleteSuccess');
-// export const deleteContactError = createAction('contacts/deleteError');
 export const addContact = createAction('contact/add');
 export const deleteContact = createAction('contact/delete');
 export const changeFilter = createAction('contact/changeFilter');
