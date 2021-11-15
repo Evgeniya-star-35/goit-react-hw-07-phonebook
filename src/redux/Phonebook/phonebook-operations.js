487883b66b691f3d79adefd6ae57288dc2e28aa3
Evgeniya Bayda <baje75128@gmail.com>
2021-11-15 12:06:22 +0200

    update responses

--- src/redux/Phonebook/phonebook-operations.js
+++ src/redux/Phonebook/phonebook-operations.js
@@ -6,26 +6,38 @@ axios.defaults.baseURL = 'https://618fa5fff6bf450017484991.mockapi.io';
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async () => {
-    const { data } = await axios.get('/contacts');
-    return data;
+    try {
+      const { data } = await axios.get('/contacts');
+      return data;
+    } catch (error) {
+      console.log(error.message);
+    }
   },
 );
 
 export const addContact = createAsyncThunk(
   'contacts/addContact',
   async contact => {
-    const { data } = await axios.post('/contacts', contact);
-    return data;
+    try {
+      const { data } = await axios.post('/contacts', contact);
+      return data;
+    } catch (error) {
+      console.log(error.message);
+    }
   },
 );
 
 export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async contactId => {
-    const {
-      data: { id },
-    } = await axios.delete(`/contacts/${contactId}`);
+    try {
+      const {
+        data: { id },
+      } = await axios.delete(`/contacts/${contactId}`);
 
-    return id;
+      return id;
+    } catch (error) {
+      console.log(error.message);
+    }
   },
 );
